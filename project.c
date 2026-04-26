f25f6796c6fdf0314affc7ff929fdec5e5d12830
banii22 <dhakalbani3612@gmail.com>
2026-04-26 23:34:27 +0545

    started with structure and array

--- project.c
+++ project.c
@@ -1,5 +1,28 @@
 #include<stdio.h>
-int main()
-{
-    printf("first commit")
-}
+#include<string.h>
+
+
+
+   printf("Hello. Welcome to my Expense Tracker.\n");
+
+   struct Expences
+   {
+    char category[30];
+    char description[50];
+    float amount;
+   };
+
+   struct Expense[50];
+   int count=0;
+
+   int main()
+   {
+
+
+    
+   }
+    
+
+git add . 
+git commit -m " made chnages in the code"
+git push origin main

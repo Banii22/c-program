4c148186262a7ba8b65f3d3eecf6d13101037e2a
banii22 <dhakalbani3612@gmail.com>
2026-04-28 23:08:14 +0545

    third commit

--- project.c
+++ project.c
@@ -1,9 +1,5 @@
 #include<stdio.h>
-#include<string.h>
 
-
-
-   printf("Hello. Welcome to my Expense Tracker.\n");
 
    struct Expences
    {
@@ -12,17 +8,60 @@
     float amount;
    };
 
-   struct Expense[50];
-   int count=0;
-
    int main()
    {
+      struct Expences e[20];
+      int count=0;
+      int choice;
+      int i, j=0;
+
+      printf("Welcome to my Expense Tracker.");
+
+      for(int i=0; i<=100; i++)
+      {
+         printf("Expense tracker\n");
+         printf("1. Add expenses.\n");
+         printf("2. View Expenses.\n");
+         printf("3.Total expense\n");
+         printf("4. Exit.\n");
+
+         printf("Enter a choice: ");
+         scanf("%d", & choice);
+
+         if(choice==1)
+         {
+            printf("Enter amount:");
+            scanf("%f", & e[j].amount);
+
+            printf("Select Category:\n");
+            printf("1. Food\n");
+            printf("2. Transportation\n");
+            printf("3. Savings\n");
+            printf("4. Entertainment\n");
+            printf("5. Education\n");
+            printf("5. MIscellaneous\n");
+
+            scanf("%d", & e[j].category);
+
+            j++;
+            
+         }
+         else if(choice==2)
+         {
+            float total=0;
+            printf("Expenses\n");
+
+            for(int i=0; i<count; i++)
+            {
+               printf("amount: 2f", e[i].amount);
+            }
+         }
+      }
 
 
-    
+
    }
-    
 
-git add . 
-git commit -m " made chnages in the code"
-git push origin main
+
+
+   

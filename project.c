f3572518ac5c81b3ce15870b434b16dd176c5a01
banii22 <dhakalbani3612@gmail.com>
2026-04-29 23:46:25 +0545

    final output 1

--- project.c
+++ project.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 
-   struct Expences
+   struct Expences                        //struct to store same type of datas.              
    {
     int category;
     float amount;
@@ -9,7 +9,7 @@
 
    int main()
    {
-      struct Expences e[20];
+      struct Expences e[20];              // to store expenses; array
       int count=0;
       int choice;
       int i, j=0;
@@ -17,7 +17,7 @@
 
       printf("Welcome to my Expense Tracker.");
 
-      for(int i=0; i<=100; i++)
+      for(int i=0; i<=100; i++)            //to keep the program running 
       {
          printf("Expense tracker\n");
          printf("1. Add expenses.\n");
@@ -28,12 +28,12 @@
          printf("Enter a choice: ");
          scanf("%d", & choice);
 
-         if(choice==1)
+         if(choice==1)             //to add expenses
          {
             printf("Enter amount:");
-            scanf("%f", & e[j].amount);
+            scanf("%f", & e[j].amount);          //storing amount sent by the user.
 
-            printf("Select Category:\n");
+            printf("Select Category:\n");           //categories for the expense management
             printf("1. Food\n");
             printf("2. Transportation\n");
             printf("3. Savings\n");
@@ -42,18 +42,19 @@
             printf("6. Miscellaneous\n");
 
             scanf("%d", & e[j].category);
-            count++;
+
+            count++;                      //to increase the count.
             j++;
             
             
          }
-         else if(choice==2)
+         else if(choice==2)                 //option:2 view expenses.
          {
-            float total=0;
+            float total=0;                   //to store total expenses.
 
             printf("Expenses\n");
 
-            for(int i=0; i<count; i++)
+            for(int i=0; i<count; i++)                  //to display all expenses.
             {
                printf("amount: %.2f || category: ", e[i].amount);
 
@@ -77,20 +78,23 @@
                }
             }
          }
-         else if(choice==3) {
+         else if(choice==3)                 //option:3 total expenses
+         {
             float total=0;
             for(i=0; i< count; i++){
-            total+= e[i].amount;
+            total+= e[i].amount;                 //adds the total.
             }
-            printf("\nYour total expence is: %.2f\n", total); 
+            printf("\nYour total expence is: %.2f\n", total);             //prints the total expenses.
          }
 
-         else if(choice==4){
+         else if(choice==4)                   // option: exiting the program
+         {
             printf("\nExiting program\n");
             break;
          }
 
-         else {
+         else                                      // if the user inputs number other than 1-4
+          {
             printf("\nInvalid choice\n");
          }
       }
